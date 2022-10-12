6df2455d00c581423c841b2dd37839bb32f14aeb
Miljan Tasevski <10219miljantasevski@etsntesla.edu.rs>
2022-10-12 10:01:21 +0200

    mornar konstruktor

---
+++ src/more/Mornar.java
@@ -0,0 +1,12 @@
+package more;
+
+public class Mornar {
+    private String ime;
+    private int kvalitet;
+    public Mornar(String ime, int kvalitet){
+        this.ime = ime;
+        this.kvalitet = kvalitet;
+    }
+
+
+}
