7ac31fa5b49434c82d91dd22f53dbb81f3cf1577
Miljan Tasevski <10219miljantasevski@etsntesla.edu.rs>
2022-10-12 10:11:24 +0200

    mornar finish

--- src/more/Mornar.java
+++ src/more/Mornar.java
@@ -8,5 +8,16 @@ public class Mornar {
         this.kvalitet = kvalitet;
     }
 
+    public String dohvIme(){
+        return this.ime;
+    }
 
+    public int dohvKvalitet(){
+        return this.kvalitet;
+    }
+
+    @Override
+    public String toString() {
+        return "M_"+ ime +  ": " + kvalitet;
+    }
 }
